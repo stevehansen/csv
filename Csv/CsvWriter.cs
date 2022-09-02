a116827926258263cb647f578fdd18d3ad6d5d15
Claas Diederichs <claas.diederichs@cd-its.de>
2022-09-02 21:57:29 +0200

    Added async methods for Write and WriteToText (#52)

--- Csv/CsvWriter.cs
+++ Csv/CsvWriter.cs
@@ -49,6 +49,46 @@ namespace Csv
                 return writer.ToString();
             }
         }
+        
+        /// <summary>
+        /// Writes the lines to the writer.
+        /// </summary>
+        /// <param name="writer">The text writer to write the data to.</param>
+        /// <param name="headers">The headers that should be used for the first line, determines the number of columns.</param>
+        /// <param name="lines">The lines with data that should be written.</param>
+        /// <param name="separator">The separator to use between columns (comma, semicolon, tab, ...)</param>
+        /// <param name="skipHeaderRow">Indicate whether the header row should be skipped, defaults to <c>false</c>.</param>
+        public static async Task  WriteAsync(TextWriter writer, string[] headers, IAsyncEnumerable<string[]> lines, char separator = ',', bool skipHeaderRow = false)
+        {
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
+            if (headers == null)
+                throw new ArgumentNullException(nameof(headers));
+            if (lines == null)
+                throw new ArgumentNullException(nameof(lines));
+
+            var columnCount = headers.Length;
+            if (!skipHeaderRow)
+                WriteLine(writer, headers, columnCount, separator);
+            await foreach (var line in lines)
+                WriteLine(writer, line, columnCount, separator);
+        }
+        
+        /// <summary>
+        /// Writes the lines and return the result.
+        /// </summary>
+        /// <param name="headers">The headers that should be used for the first line, determines the number of columns.</param>
+        /// <param name="lines">The lines with data that should be written.</param>
+        /// <param name="separator">The separator to use between columns (comma, semicolon, tab, ...)</param>
+        /// <param name="skipHeaderRow">Indicate whether the header row should be skipped, defaults to <c>false</c>.</param>
+        public static async Task<string> WriteToTextAsync(string[] headers, IAsyncEnumerable<string[]> lines, char separator = ',', bool skipHeaderRow = false)
+        {
+            using (var writer = new StringWriter())
+            {
+                await WriteAsync(writer, headers, lines, separator, skipHeaderRow);
+                return writer.ToString();
+            }
+        }
 
         private static void WriteLine(TextWriter writer, string[] data, int columnCount, char separator)
         {
